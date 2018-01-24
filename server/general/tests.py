ea49364935879ccd9908943783cc9d0cf95e5ea5
Sergio Perea <sperea@gmail.com>
2018-01-24 18:00:45 +0100

    tests

--- server/general/tests.py
+++ server/general/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from general.models import SpaUser
 
-# Create your tests here.
+# models test
+class SpaUserTest(TestCase):
+
+    def create_SpaUser(self, email="email@email.com", password="jlaasociados"):
+        return SpaUser.objects.create(email=email, password=password)
+
+    def test_SpaUser_creation(self):
+        miusuario = self.create_SpaUser()
+        self.assertTrue(isinstance(miusuario, SpaUser))
+        self.assertEqual(miusuario.__str__(), miusuario.email)
--- server/server/settings.py
+++ server/server/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'general',
+    'coverage',
 ]
 
 MIDDLEWARE = [
